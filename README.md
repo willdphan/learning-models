882ac8eb9c303e70b56e8914dbc7302f5339e48c
William Phan <willdphan@gmail.com>
2023-03-30 18:28:16 -0400

    readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# Learning Models
+
+A repo used to learn the basics of optimization algorithms used to train ML models. Also goes over the basics of PyTorch. Comments are written out step by step for learning purposes.
+
+## Installation
+
+To install `Gradio` and `OpenAI`, use the commands below.
+
+    pip3 install torch
+    pip3 install numpy
+    pip3 install math
+    pip3 install torchvision
+    pip3 install sklearn
+
+Or just use this command to install everything for you.
+
+    pip3 install -r requirements.txt
+
+## Overview
+
+The Repo goes over the basics of PyTorch Syntax. While using PyTorch, it goes over different algorithms such as:
+
+    Linear Regression
+    Logistic Regression
+
+## License
+
+This script is open-source and licensed under the MIT License. For more details, check the [LICENSE](LICENSE) file.
---
+++ dataloader.py
@@ -0,0 +1,111 @@
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import math
+
+# gradient computation etc. not efficient for whole data set
+# -> divide dataset into small batches
+
+'''
+# training loop
+for epoch in range(num_epochs):
+    # loop over all batches
+    for i in range(total_batches):
+        batch_x, batch_y = ...
+'''
+
+# epoch = one forward and backward pass of ALL training samples
+# batch_size = number of training samples used in one forward/backward pass
+# number of iterations (samples) = number of passes, each pass (forward+backward) using [batch_size] number of sampes
+# e.g : 100 samples, batch_size=20 -> 100/20=5 iterations for 1 epoch
+
+# --> DataLoader can do the batch computation for us
+
+# Implement a custom Dataset:
+# inherit Dataset
+# implement __init__ , __getitem__ , and __len__
+
+class WineDataset(Dataset):
+
+    def __init__(self):
+        # Initialize data, download, etc.
+        # read with numpy or pandas
+        # delimeter = ',' because its a comma separated file
+        # skip the first row since its the header
+        xy = np.loadtxt('./data/wine/wine.csv', delimiter=',', dtype=np.float32, skiprows=1)
+        self.n_samples = xy.shape[0]
+
+        # here the first column is the class label, the rest are the features
+        # from_numpy() creates a tensor from a numpy array
+        # we want all the samples except the first column
+        self.x_data = torch.from_numpy(xy[:, 1:]) # size [n_samples, n_features]
+        # we want all the samples including the first column
+        self.y_data = torch.from_numpy(xy[:, [0]]) # size [n_samples, 1]
+
+    # support indexing such that dataset[i] can be used to get i-th sample
+    # can get the item at a specific index
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    # we can call len(dataset) to return the size/the number of samples
+    def __len__(self):
+        return self.n_samples
+
+
+# create/initialize dataset
+dataset = WineDataset()
+
+# get first sample and unpack
+# indexes fist sample
+first_data = dataset[0]
+# get its features and labels
+features, labels = first_data
+# print its features and labels
+print(features, labels)
+
+# Load whole dataset with DataLoader
+# shuffle: shuffle data, good for training
+# num_workers: faster loading with multiple subprocesses
+# !!! IF YOU GET AN ERROR DURING LOADING, SET num_workers TO 0 !!!
+train_loader = DataLoader(dataset=dataset,
+                          batch_size=4,
+                          shuffle=True,
+                          num_workers=2)
+
+# convert to an iterator and look at one random sample
+dataiter = iter(train_loader)
+data = next(dataiter)
+features, labels = data
+print(features, labels)
+
+# Dummy Training loop
+num_epochs = 2
+total_samples = len(dataset)
+n_iterations = math.ceil(total_samples/4)
+print(total_samples, n_iterations)
+for epoch in range(num_epochs):
+    for i, (inputs, labels) in enumerate(train_loader):
+        
+        # here: 178 samples, batch_size = 4, n_iters=178/4=44.5 -> 45 iterations
+        # Run your training process
+        if (i+1) % 5 == 0:
+            print(f'Epoch: {epoch+1}/{num_epochs}, Step {i+1}/{n_iterations}| Inputs {inputs.shape} | Labels {labels.shape}')
+
+# some famous datasets are available in torchvision.datasets
+# e.g. MNIST, Fashion-MNIST, CIFAR10, COCO
+
+train_dataset = torchvision.datasets.MNIST(root='./data', 
+                                           train=True, 
+                                           transform=torchvision.transforms.ToTensor(),  
+                                           download=True)
+
+train_loader = DataLoader(dataset=train_dataset, 
+                                           batch_size=3, 
+                                           shuffle=True)
+
+# look at one random sample
+dataiter = iter(train_loader)
+data = next(dataiter)
+inputs, targets = data
+print(inputs.shape, targets.shape)
